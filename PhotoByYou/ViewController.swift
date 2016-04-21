eef574b16932a81b2946fa0f6de4117c6f3ba568
Dung Vu <dung.vu@audiencemedia.com>
2016-04-21 11:10:35 +0700

    Initial Commit

---
+++ PhotoByYou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoByYou
+//
+//  Created by Dung Vu on 4/21/16.
+//  Copyright © 2016 Dung Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
